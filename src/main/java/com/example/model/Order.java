9e241a29949b8c0446792045ba6e8806c00e0abb
k-i-k-a <100940677+k-i-k-a@users.noreply.github.com>
2025-02-28 20:36:59 +0200

    Finished Order handling, Still require validation in Service.

--- src/main/java/com/example/model/Order.java
+++ src/main/java/com/example/model/Order.java
@@ -1,4 +1,21 @@
 package com.example.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Component
 public class Order {
+    private UUID id;
+    private UUID userId;
+    private double totalPrice;
+    private List<Product> products = new ArrayList<>();
 }
